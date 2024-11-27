66b82ae3d2994a99bbbfa5b7c13d58924c94bb89
CarlosQuiroz25 <caquirozm@cesde.net>
2024-11-27 12:20:48 -0500

    Mejoras en el apartado dinámico de los trainers

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -19,3 +19,22 @@ prev.addEventListener('click', () => {
   index = (index - 1 + images.length) % images.length;
   updateCarousel();
 });
+
+document.getElementById("entrenador1").addEventListener("click", () => {
+  window.open("https://www.instagram.com/yei_aven19/", "_blank")
+})
+document.getElementById("entrenador2").addEventListener("click", () => {
+  window.open("https://www.instagram.com/juanes_ac17/", "_blank")
+})
+document.getElementById("entrenador3").addEventListener("click", () => {
+  window.open("https://www.instagram.com/afserna_70/", "_blank")
+})
+document.getElementById("entrenador4").addEventListener("click", () => {
+  window.LargestContentfulPaint("https://www.instagram.com/r_a_cristian/", "_blank")
+})
+document.getElementById("entrenador5").addEventListener("click", () => {
+  window.open("https://www.instagram.com/c_ossa/", "_blank")
+})
+document.getElementById("entrenador6").addEventListener("click", () => {
+  window.open("https://www.instagram.com/alejoparra92/", "_blank")
+})
